9b77db74f2f68fd08f97b9cf8dc212901586aaaa
hudatumoe <dgy@dgy.xyz>
2023-01-22 13:59:06 +0800

    update github action

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+build(){
+    exe=""
+    if [ $GOOS == windows ] ; then exe=".exe" ; fi
+
+    # shellcheck disable=SC2027
+    echo "正在编译: "$GOOS"_"$GOARCH
+    go build -trimpath -ldflags "-w -s" -o  "NekoXProxy_""$GOOS"_"$GOARCH""$exe"
+}
+rm -rf releases
+mkdir releases
+mv NekoXProxy_* releases
